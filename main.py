98e48a2116228059b5210040e3e1ac6ce65d6d11
piadozo-edriane <doidoiwey@gmail.com>
2024-12-18 11:59:42 +0800

    Add main menu. -Piadozo

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+
+UNSET_OPTION = " "
+EXIT_OPTION = "0"
+
+def menu():
+    user_choice = UNSET_OPTION
+    while user_choice != EXIT_OPTION:
+        user_choice = display_choice()
+        process_choice(user_choice)
+
+def display_choice():
+    os.system('cls')
+    print("===============> Fire Ram <===============")
+    print("1. Edriane O. Piadozo")
+    print("2. Kirby G. Consultado")
+    print("3. Gener A. Andaya Jr. ")
+    print("4. Kevin Joseph V. Barcelos")
+    print("5. Marc D. Veslino")
+    print("0. Exit ")
+    print("===============> <======> <===============")
+    
+    return input("Enter your choice: ")
+
+def process_choice(user_choice):
+    match user_choice:
+        case '1':
+            pass
+        case '2':
+            pass
+        case '3':
+            pass
+        case '4':
+            pass
+        case '5':
+            pass
+        case '0':
+            pass
+        case _:
+            input("Invalid input.")
+
+menu()
